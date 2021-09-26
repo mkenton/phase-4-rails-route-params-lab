47f20a67650fdcafbaa4bdcac64a8241f83a3d97
Michael <mkenton2020@icloud.com>
2021-09-25 22:10:22 -0500

    commit tree 66d09683109b78a148fa6257d05356ed07c0479e

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,13 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+
+  def highest_grade
+    student = Student.order(grade: :desc).first
+    render json: student
+  end
 end
